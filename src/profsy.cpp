b64955dcf8ade0fa15e02082d84f8269b5587a7d
wc-duck <Fredrik.Kihlander@gmail.com>
2012-10-22 21:56:47 +0200

    if profiler runs out of available scopes, a default overflow-scope will be used to track these

--- src/profsy.cpp
+++ src/profsy.cpp
@@ -4,6 +4,8 @@
 
 #define ALIGN_UP( in, alignment ) (size_t)( ( (size_t)(in) + (size_t)(alignment) - 1 ) & ~( (size_t)(alignment) - 1 ) )
 
+const unsigned int PROFSY_BUILTIN_SCOPES = 2; // "root"- and "overflow"-scopes
+
 struct profsy_entry
 {
 	profsy_scope_data data;
@@ -24,7 +26,8 @@ struct profsy_ctx
 	unsigned int  entries_max;
 
 	profsy_entry* root;
-	profsy_entry* current; // TODO: need to be per-thread later
+	profsy_entry* overflow; // All scopes allocated if we run out of entries will get registered in this scope
+	profsy_entry* current;  // TODO: need to be per-thread later
 
 	uint64_t frame_start;
 };
@@ -34,13 +37,15 @@ static profsy_ctx* g_profsy_ctx;
 static profsy_entry* profsy_alloc_entry( profsy_ctx_t ctx, const char* name )
 {
 	if( ctx->entries_used >= ctx->entries_max )
-		return 0x0;
+		return ctx->overflow;
 	
 	profsy_entry* entry = ctx->entries + ctx->entries_used++;
 	
 	entry->parent              = 0x0;
 	entry->children            = 0x0;
 	entry->next_child          = 0x0;
+	entry->calls               = 0;
+	entry->time                = 0;
 	
 	entry->data.name           = name;
 	entry->data.depth          = 0;
@@ -55,7 +60,7 @@ size_t profsy_calc_ctx_mem_usage( const profsy_init_params* params )
 	
 	needed_mem += sizeof( profsy_ctx ); 
 	needed_mem  = ALIGN_UP( needed_mem, 16 );
-	needed_mem += params->entries_max * sizeof( profsy_entry );
+	needed_mem += ( params->entries_max + PROFSY_BUILTIN_SCOPES ) * sizeof( profsy_entry ); // + 2 for "root" and "overflow"
 	
 	return needed_mem;
 }
@@ -73,19 +78,19 @@ void profsy_init( const profsy_init_params* params, uint8_t* mem )
 	
 	ctx->entries      = ( profsy_entry* )mem;
 	ctx->entries_used = 0;
-	ctx->entries_max  = params->entries_max;
+	ctx->entries_max  = params->entries_max + PROFSY_BUILTIN_SCOPES;
 	
 	memset( ctx->entries, 0x0, sizeof( profsy_entry ) * ctx->entries_max );
 
 	// create root scope here!
 	profsy_entry* root = profsy_alloc_entry( ctx, "root" );
+	profsy_entry* over = profsy_alloc_entry( ctx, "overflow scope" );
 
-	// TODO: remove hack
-	root->calls = 0; // TODO: Hack root to be one call
-	root->time  = 0; // TODO: Hack root to be one call
+	over->parent  = root;
 
-	ctx->root    = root;
-	ctx->current = root;
+	ctx->root     = root;
+	ctx->overflow = over;
+	ctx->current  = root;
 
 	ctx->frame_start = PROFSY_CUSTOM_TICK_FUNC();
 	
@@ -107,13 +112,15 @@ profsy_ctx_t profsy_global_ctx()
 
 // add functions to alloc scopes outside of macro
 
-static profsy_entry* profsy_get_child_scope( profsy_entry* parent, const char* name )
+static profsy_entry* profsy_get_child_scope( profsy_ctx* ctx, profsy_entry* parent, const char* name )
 {
 	profsy_entry* e = parent->children;
-	while( e != 0x0 && e->data.name != name )
+	while( e > ctx->overflow && e->data.name != name )
 		e = e->next_child;
 	return e;
 }
+
+#include <stdio.h>
 
 int profsy_scope_enter( const char* name )
 {
@@ -125,7 +132,7 @@ int profsy_scope_enter( const char* name )
 	profsy_entry* current = ctx->current;
 
 	// search for scope in current open scope
-	profsy_entry* e = profsy_get_child_scope( current, name );
+	profsy_entry* e = profsy_get_child_scope( ctx, current, name );
 
 	// not found!
 	if( e == 0x0 )
@@ -133,22 +140,11 @@ int profsy_scope_enter( const char* name )
 		// TODO: take lock here if multiple threads are to be supported
 
 		// search again since some other thread might have created the scope!
-		e = profsy_get_child_scope( current, name );
+		e = profsy_get_child_scope( ctx, current, name );
 		if( e == 0x0 )
 		{
 			// alloc scope and link
 			e = profsy_alloc_entry( ctx, name );
-			// ASSERT( e != 0x0, "out of entries! scope: %s", name );
-
-			e->data.depth = (uint16_t)(current->data.depth + 1);
-			e->parent = current;
-
-			profsy_entry* parent = e->parent;
-			while( parent != 0 )
-			{
-				parent->data.num_sub_scopes++;
-				parent = parent->parent;
-			}
 
 			// insert at tail to get order where scopes was registered.
 			if( current->children )
@@ -160,8 +156,21 @@ int profsy_scope_enter( const char* name )
 			}
 			else
 			{
-				e->next_child = 0x0;
-				current->children = e;
+				if( current != ctx->overflow )
+					current->children = e;
+			}
+
+			if( e == ctx->overflow )
+				return (int)( e - ctx->entries );
+
+			e->data.depth = (uint16_t)(current->data.depth + 1);
+			e->parent = current;
+
+			profsy_entry* parent = e->parent;
+			while( parent != 0 )
+			{
+				parent->data.num_sub_scopes++;
+				parent = parent->parent;
 			}
 		}
 	}
--- test/profsy_tests.cpp
+++ test/profsy_tests.cpp
@@ -2,6 +2,8 @@
 
 #include <profsy/profsy.h>
 
+#define ARRAY_LENGTH(a) (sizeof(a)/sizeof(a[0]))
+
 TEST( basic, setup_teardown )
 {
 	profsy_init_params ip;
@@ -24,12 +26,11 @@ TEST( basic, setup_teardown )
 	free( mem );
 }
 
-struct profsy : public ::testing::Test
+template<unsigned int ENTRIES_MAX>
+struct profsy_ : public ::testing::Test
 {
-	static const unsigned int ENTRIES_MAX;
-
 	virtual void SetUp()
-{
+	{
 		profsy_init_params ip;
 		ip.entries_max = ENTRIES_MAX;
 		
@@ -43,7 +44,7 @@ struct profsy : public ::testing::Test
 		EXPECT_NE( (profsy_ctx_t)0, profsy_global_ctx() );
 		EXPECT_EQ( profsy_global_ctx(), profsy_global_ctx() );
 
-		EXPECT_EQ( profsy_num_active_scopes(), 1u );
+		EXPECT_EQ( profsy_num_active_scopes(), 2u );
 	}
 
 	virtual void TearDown()
@@ -57,15 +58,16 @@ struct profsy : public ::testing::Test
 	uint8_t* mem;
 };
 
-const unsigned int profsy::ENTRIES_MAX = 256;
+typedef profsy_<4>   profsy_4;
+typedef profsy_<256> profsy;
 
 TEST_F( profsy, active_scopes_at_init )
 {
 	// wcprof was setup with max ENTRIES_MAX scopes
-	EXPECT_EQ( ENTRIES_MAX, profsy_max_active_scopes() );
+	EXPECT_EQ( 258, profsy_max_active_scopes() );
 
-	// check that only root-scope is allocated
-	EXPECT_EQ( 1,           profsy_num_active_scopes() );
+	// check that only root- and overflow-scope is allocated
+	EXPECT_EQ( 2,           profsy_num_active_scopes() );
 }
 
 TEST_F( profsy, simple_scope_alloc )
@@ -74,13 +76,13 @@ TEST_F( profsy, simple_scope_alloc )
 		PROFSY_SCOPE( "test-scope1" );
 		for( int i = 0; i < 1000; ++i ); // replace with sleep()
 	}
-	// check that only root-scope + new one is allocated
-	EXPECT_EQ( 2, profsy_num_active_scopes() );
+	// check that only root, overflow-scope + new one is allocated
+	EXPECT_EQ( 3, profsy_num_active_scopes() );
 
 	// swap frame to calculate "stuff"
 	profsy_swap_frame();
 
-	EXPECT_EQ( profsy_num_active_scopes(), 2u );
+	EXPECT_EQ( profsy_num_active_scopes(), 3u );
 
 	// check that scope was entered and left
 	const profsy_scope_data* hierarchy[16];
@@ -129,13 +131,13 @@ TEST_F( profsy, deep_hierarchy )
 	// swap frame to calculate "stuff"
 	profsy_swap_frame();
 
-	EXPECT_EQ( profsy_num_active_scopes(), 9u );
+	EXPECT_EQ( profsy_num_active_scopes(), 10u );
 
 	// check that scope was entered and left
 	const profsy_scope_data* hierarchy[16];
 	profsy_get_scope_hierarchy( hierarchy, 16 );
 
-	for( unsigned int i = 0; i < 9; ++i )
+	for( unsigned int i = 0; i < ARRAY_LENGTH(SCOPE_NAMES); ++i )
 	{
 		const profsy_scope_data* scope = hierarchy[i];
 		EXPECT_STREQ( scope->name,           SCOPE_NAMES[i] );
@@ -144,7 +146,7 @@ TEST_F( profsy, deep_hierarchy )
 		EXPECT_EQ   ( scope->depth,          i );
 		EXPECT_EQ   ( scope->num_sub_scopes, 8u - i );
 
-		if( i == 8 )
+		if( i == ARRAY_LENGTH(SCOPE_NAMES) - 1 )
 			EXPECT_EQ( scope->child_time, 0u );
 		else
 			EXPECT_GT( scope->child_time, 0u );
@@ -182,7 +184,7 @@ TEST_F( profsy, two_paths )
 	// swap frame to calculate "stuff"
 	profsy_swap_frame();
 
-	EXPECT_EQ( profsy_num_active_scopes(), 7u ); // root + parent_scope1 + parent_scope2 + 2 * scoped_func2 + 2 * scoped_func1
+	EXPECT_EQ( profsy_num_active_scopes(), 8u ); // root + overflow + parent_scope1 + parent_scope2 + 2 * scoped_func2 + 2 * scoped_func1
 
 	const profsy_scope_data* hierarchy[16];
 	profsy_get_scope_hierarchy( hierarchy, 16 );
@@ -216,6 +218,74 @@ TEST_F( profsy, two_paths )
 	s = hierarchy[6];
 	EXPECT_STREQ( s->name,  "scoped_func2" );
 	EXPECT_EQ   ( s->depth, 2u );
+}
+
+TEST_F( profsy_4, out_of_resources_is_tracked )
+{
+	{
+		PROFSY_SCOPE("s1");
+		PROFSY_SCOPE("s2");
+		PROFSY_SCOPE("s3");
+		PROFSY_SCOPE("s4");
+		PROFSY_SCOPE("s5"); // this scope will overflow...
+		PROFSY_SCOPE("s6"); // this scope will overflow...
+	}
+
+	profsy_swap_frame();
+	EXPECT_EQ( 6u, profsy_num_active_scopes() ); // max amount of scopes active...
+
+	const profsy_scope_data* hierarchy[16];
+	profsy_get_scope_hierarchy( hierarchy, 16 );
+
+	const profsy_scope_data* s;
+	s = hierarchy[0];
+	EXPECT_STREQ( s->name,  "root" );
+	EXPECT_EQ   ( s->calls, 1u );
+
+	s = hierarchy[1]; EXPECT_STREQ( s->name,  "s1" ); EXPECT_EQ( s->calls, 1u );
+	s = hierarchy[2]; EXPECT_STREQ( s->name,  "s2" ); EXPECT_EQ( s->calls, 1u );
+	s = hierarchy[3]; EXPECT_STREQ( s->name,  "s3" ); EXPECT_EQ( s->calls, 1u );
+	s = hierarchy[4]; EXPECT_STREQ( s->name,  "s4" ); EXPECT_EQ( s->calls, 1u );
+
+	s = hierarchy[5];
+	EXPECT_STREQ( s->name,  "overflow scope" );
+	EXPECT_EQ   ( s->calls, 2u ); // 2 calls, "s3" and "s4"
+}
+
+static void test_it( bool do_scope )
+{
+	if( do_scope )
+	{
+		PROFSY_SCOPE( "s5" );
+	}
+}
+
+TEST_F( profsy_4, multi_overflow )
+{
+	{
+		for( int i = 0; i < 2; ++i )
+		{
+			{ PROFSY_SCOPE("s1"); test_it( i > 0 ); }
+			{ PROFSY_SCOPE("s2"); test_it( i > 0 ); }
+			{ PROFSY_SCOPE("s3"); test_it( i > 0 ); }
+		}
+	}
+
+	profsy_swap_frame();
+	EXPECT_EQ( 6u, profsy_num_active_scopes() ); // max amount of scopes active...
+
+	const profsy_scope_data* hierarchy[16];
+	profsy_get_scope_hierarchy( hierarchy, 16 );
+
+	const profsy_scope_data* s;
+	s = hierarchy[0]; EXPECT_STREQ( s->name,  "root" ); EXPECT_EQ( s->calls, 1u );
+	s = hierarchy[1]; EXPECT_STREQ( s->name,    "s1" ); EXPECT_EQ( s->calls, 2u );
+	s = hierarchy[2]; EXPECT_STREQ( s->name,    "s5" ); EXPECT_EQ( s->calls, 1u );
+	s = hierarchy[3]; EXPECT_STREQ( s->name,    "s2" ); EXPECT_EQ( s->calls, 2u );
+	s = hierarchy[5]; EXPECT_STREQ( s->name,    "s3" ); EXPECT_EQ( s->calls, 2u );
+
+	EXPECT_EQ( hierarchy[4], hierarchy[6] );
+	s = hierarchy[4]; EXPECT_STREQ( s->name,  "overflow scope" ); EXPECT_EQ( s->calls, 2u ); // 2 calls, "s2->s5" and "s3->s5"
 }
 
 
